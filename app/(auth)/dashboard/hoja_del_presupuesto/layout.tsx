8492aad70a749a143e1d05006852a707d38a63bb
cuevacelis <cuevacelis@hotmail.com>
2024-10-17 17:47:26 -0500

    fix: types error

--- app/(auth)/dashboard/hoja_del_presupuesto/layout.tsx
+++ app/(auth)/dashboard/hoja_del_presupuesto/layout.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 interface IPropsLayout {
   children: React.ReactNode;
-  modal: React.ReactNode;
+  // modal: React.ReactNode;
 }
 
-export default function ProyectosLayout({ children, modal }: IPropsLayout) {
+export default function ProyectosLayout({ children }: IPropsLayout) {
   return (
     <>
       {children}
-      {modal}
+      {/* {modal} */}
     </>
   );
 }
