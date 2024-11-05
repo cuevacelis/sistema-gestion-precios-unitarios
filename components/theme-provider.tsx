639d9dc41a8273465ff36329929e87f9652b2a71
cuevacelis <cuevacelis@hotmail.com>
2024-11-04 20:31:14 -0500

    fix: build

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+
+interface ThemeProviderProps
+  extends React.ComponentProps<typeof NextThemesProvider> {
+  children: React.ReactNode;
+}
 
 export default function ThemeProvider({
   children,
